fe52e66033ff338d6fd081e4bcc407aeef247e39
Rodrigo Oliveira <rodrigooliveira@Rodrigos-MacBook-Pro.local>
2024-05-24 06:02:35 -0300

    Updated nameValidator

--- src/app/shared/validators/name.validator.ts
+++ src/app/shared/validators/name.validator.ts
@@ -1,7 +1,7 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 function isValidName(value: string):boolean {
-  return new RegExp('^[a-zA-Z]+(\\s[a-zA-Z]+)+$').test(value);
+  return new RegExp('^[a-zA-ZÀ-ÖØ-öø-ÿ]+(\\s[a-zA-ZÀ-ÖØ-öø-ÿ]+)+$').test(value);
 }
 
 export function nameValidator(control: AbstractControl): ValidationErrors | null  {
